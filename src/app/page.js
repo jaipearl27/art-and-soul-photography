815c213b73fd55c37155517bd73addab2990fe94
Anjali Bartwal <anjalibartwal@pearlorganisation.com>
2024-10-23 17:24:57 +0530

    vedio section

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import { Hero } from "@/components/Hero";
 import WeddingList from "@/components/WeddingList";
+import ModernApproach from "./modern-approach/modernApproach";
+import VedioSec from "./vedio-sec/VedioSec";
 
 export default function Home() {
   return (
@@ -11,6 +13,8 @@ export default function Home() {
         
       />
       <WeddingList />
+      <ModernApproach/>
+      <VedioSec/>
     </>
   );
 }
